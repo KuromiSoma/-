a0bbe5364720e3775c53420b073147af7c2a5fe9
KuromiSoma <tamagoyaki.zyukuzyuku@gmail.com>
2023-06-15 18:54:46 +0900

    色々追加

--- Hero.java
+++ Hero.java
@@ -1,30 +1,78 @@
+import java.util.Scanner;
+
 public class Hero {
     String name;
+    int maxHp;
     int hp;
     int attack;
     int speed;
+    int EXP;
     
-    Hero(String a, int b, int c, int d){
+    Hero(String a, int b, int c, int d, int e){
         name = a;
-        hp = b;
-        attack = c;
-        speed = d;
+        maxHp = b;
+        hp = c;
+        attack = d;
+        speed = e;
+
     }
+
+    public void status(){
+        System.out.println("ステータス");
+        System.out.println(this.name);
+        System.out.println("【ＨＰ】" + this.hp + " / " + this.maxHp);
+        System.out.println("【攻撃力】" + this.attack);
+        System.out.println("【スピード】" + this.speed);
+    }
+
+    public void Experience(int OriEXP){
+        EXP += OriEXP;
+        System.out.println("増やしたいステータスを選択してください");
+
+        boolean validInputEx1 = false; // 1か2か3を入力させるため
+
+     while (!validInputEx1) {
+        System.out.println("1 : 最大ＨＰ");
+        System.out.println("2 : 攻撃力");
+        System.out.println("3 : スピード");
+
+        Scanner scanner = new Scanner(System.in);
+        int whatUp = scanner.nextInt();
+        scanner.close();
+
+        switch(whatUp){
+            case 1:
+            this.hp += EXP;
+            System.out.println("最大ＨＰが" + this.EXP + "上がった！");
+            validInputEx1 = true;
+            break;
+
+            case 2:
+            this.attack += EXP;
+            System.out.println("攻撃力が" + this.EXP + "上がった！");
+            validInputEx1 = true;
+            break;
+
+            case 3:
+            this.speed += EXP;
+            System.out.println("スピード" + this.EXP + "上がった！");
+            validInputEx1 = true;
+            break;
+
+            default:
+            System.out.println("再度入力してください");
+            break;
+        }
+     }
+
+     status();
+
+     System.out.println("Enterキーを押して次に進んでください");
+        
+     Scanner scanner = new Scanner(System.in);
+     scanner.nextLine(); // Enterキーが押されるまで待機
+     scanner.close();
     
-    public void sleep(){
-        this.hp = 100;
-        System.out.println(this.name + "は、眠って回復した");
     }
 
-    public void sit(int sec){
-        this.hp += sec;
-        System.out.println(this.name + "は、" + "秒座った。");
-        System.out.println("HPが" + sec + "ポイント回復した");
-    }
-
-    public void run(){
-        System.out.println(this.name + "は、逃げ出した");
-        System.out.println("GAMEOVER");
-        System.out.println("最終HPは" + this.hp + "でした。");
-    }
 }
--- Story_01.java
+++ Story_01.java
@@ -2,18 +2,28 @@ import java.util.Scanner;
 
 public class Story_01 {
 
+    private static Hero h;
+    private static Monster sa;
+
     public static void s0(){
     System.out.println("あなたの名前を入力してください");
     Scanner scanner = new Scanner(System.in);
     String heroName = scanner.next();
+    scanner.close();
 
      // 勇者を召喚
-    Hero h = new Hero("勇者"+heroName, 100, 25, 30);
+    h = new Hero("勇者"+heroName, 100, 100, 25, 30);
 
     System.out.println(h.name + "を生み出しました。");
-    System.out.println(h.name + "のHPは" + h.hp + "です");
-    System.out.println(h.name + "の攻撃力は" + h.attack + "です");
-    System.out.println(h.name + "のHPは" + h.speed + "です");
+    System.out.println("【最大ＨＰ】" + h.maxHp);
+    System.out.println("【攻撃力】" + h.attack);
+    System.out.println("【スピード】" + h.speed);
+
+    System.out.println("Enterキーを押して次に進んでください");
+        
+    Scanner scanner0 = new Scanner(System.in);
+    scanner0.nextLine(); // Enterキーが押されるまで待機
+    scanner0.close();
 
     // 行き先を選ぶ
     System.out.println("どこへ向かおうか？");
@@ -25,6 +35,7 @@ public class Story_01 {
 
         Scanner scanner2 = new Scanner(System.in);
         int choice02 = scanner2.nextInt(); // 入力を受け取る
+        scanner2.close();
 
         switch (choice02) {
             case 1:
@@ -34,6 +45,7 @@ public class Story_01 {
     
             case 2:
                 s1_2();
+                s1_1();
                 validInput01 = true; // 入力が正しい場合はvalidInput01をtrueに設定してループを終了
                 break;
     
@@ -47,14 +59,126 @@ public class Story_01 {
 
     public static void s1_1(){
     // スライムを召喚
-    Monster sa = new Monster("スライムA", 30, 5, 15);
+    sa = new Monster("スライムA", 30, 5, 15);
     System.out.println(sa.name + "が現れた！");
+    System.out.println("【戦闘開始】");
+
+    System.out.println("Enterキーを押して次に進んでください");
+        
+    Scanner scanner0a = new Scanner(System.in);
+    scanner0a.nextLine(); // Enterキーが押されるまで待機
+    scanner0a.close();
+
+    if(h.speed >= sa.speed) {
+        System.out.println(h.name + "の行動");
+        System.out.println("");
+
+        boolean validInput02 = false; // 1か2を入力させるため
+
+    while (!validInput02) {
+
+        System.out.println("1:攻撃 2:逃げる");
+        System.out.println("");
+
+        Scanner scanner3 = new Scanner(System.in);
+        int choice03 = scanner3.nextInt(); // 入力を受け取る
+        scanner3.close();
+
+        switch (choice03) {
+            case 1: // 戦う場合
+                System.out.println(h.name + "の攻撃");
+                System.out.println("");
+                sa.hp -= h.attack;
+                System.out.println(sa.name + "に" + h.attack + "ダメージ");
+                System.out.println("");
+                if(sa.hp <= 0){
+                    System.out.println(sa.name + "が倒れた");
+
+                    System.out.println("Enterキーを押して次に進んでください");
+                    scanner0a.nextLine(); // Enterキーが押されるまで待機
+
+                    System.out.println(h.name + "は経験値を 10 獲得した！");
+                    h.Experience(10);
+                    
+
+
+                    validInput02 = true; // 敵を倒すとtrueに設定してループを終了
+                }else{
+                    System.out.println(sa.name + "の残りHP" + sa.hp);
+                    System.out.println(sa.name + "の攻撃をどう受ける？");
+                    System.out.println("");
+
+                    System.out.println("1:横に飛んで回避 2:後ろに飛んで回避");
+                    System.out.println("");
+
+                    boolean validInput3 = false; // 1か2を入力させるため
+
+                    while(!validInput3) {
+
+                    Scanner scanner4 = new Scanner(System.in);
+                    int choice04 = scanner4.nextInt(); // 入力を受け取る
+                    scanner4.close();
+
+
+                    switch (choice04) {
+                        case 1:
+                        System.out.println(sa.name + "の攻撃をかわした！");
+                        System.out.println("");
+                        validInput3 = true;
+                        break;
+
+                        case 2:
+                        System.out.println(sa.name + "の攻撃をくらった！");
+                        h.hp -= sa.attack;
+                        System.out.println(h.name + "の残りHP" + h.hp);
+                        System.out.println("");
+                        validInput3 = true;
+                        break;
+
+                        default:
+                        System.out.println("再度入力してください");
+                        break;
+                    }
+
+                    }
+                    
+
+                }
+                break;
+    
+            case 2: // 逃げる場合
+                validInput02 = true; // trueに設定してループを終了
+
+                System.out.println(h.name + "は逃げ出した。");
+
+                scanner0a.nextLine(); // Enterキーが押されるまで待機
+                System.out.println("");
+                break;
+    
+            default:
+                System.out.println("再度入力してください");
+                break;
+        }
+    }
+    }
 
     }
 
     public static void s1_2(){
     //　釣りを始める
-    System.out.println("川釣りをして魚をゲットしましょう");
+    System.out.println("川釣りをして魚をゲットした！");
+    System.out.println(h.name + "の最大HPが 10 増えた！" );
+    h.maxHp += 10;
+    h.hp += 10;
+    h.status();
+    System.out.println("");
+    System.out.println("魔物の森へ移動します");
+    System.out.println("Enterキーを押して次に進んでください");
+        
+    Scanner scanner0b = new Scanner(System.in);
+    scanner0b.nextLine(); // Enterキーが押されるまで待機
+    scanner0b.close();
+    System.out.println("");
     }
 
 }
